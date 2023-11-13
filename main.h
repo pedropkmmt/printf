7dc6860ec8c5164e08c34f4fdae73a91e7a35bed
pedro <pedropkmmt@gmail.com>
2023-11-13 03:46:25 -0800

    task 0

--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
+int _printf_char(va_list val);
 int _printf(const char *format, ...);
+int printf_string(va_list, val);
+int _strlen(char *str);
+int _strlent(const char *str);
+int printf_37(void);
 #endif
---
+++ printf_%.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * printf_37 - main function
+ * Return: integer
+ */
+int printf_37(void)
+{
+	_putchar(37);
+	return (1);
+}
