8f7d9efac11994ea0b78832679e7d36e12c5b1c1
pedro <pedropkmmt@gmail.com>
2023-11-16 12:25:02 -0800

    fixes

--- main.h
+++ main.h
@@ -19,25 +19,8 @@ typedef struct format
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void print_formit(char formit[], int *form_ind);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _strlen(char *str);
-int _strlent(const char *str);
-int print_percen(void);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int printf_bin(va_list val);
-int printf_unisgned(va_list args);
-int print_o(va_list val);
-int printf_x(va_list val);
-int printf_X(va_list val);
-int print_X_t(unsigned int num);
-int printf_ts(va_list val);
-int print_x_t(unsigned long int num);
-int printf_pointer(va_list val);
-int printf_r(va_list val);
-int printf_rot(va_list val);
+void print_buffer(char buffer[], int *buff_ind);
+
 
 #endif
 
