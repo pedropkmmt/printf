c0bd1c7a83080512dd9af0344cc68b4843a953ca
pedro <pedropkmmt@gmail.com>
2023-11-17 13:05:53 -0800

    fixes

--- main.h
+++ main.h
@@ -1,15 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <limits.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-/**
- * struct format - main struct
- * @ps: pointer
- * @functtion: function
- */
 
 int _putchar(char c);
 int _printf(const char *format, ...);
