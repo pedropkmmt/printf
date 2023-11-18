3c581ed8b3086385e7da9e7eafcc5ba650c9b8f9
pedro <pedropkmmt@gmail.com>
2023-11-17 18:30:29 -0800

    fix

--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
 int print_char(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_percent(va_list types, char buffer[],int flags, int width, int precision, int size)
+
 #endif
