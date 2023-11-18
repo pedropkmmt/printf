f8e3eaebcc12c007a02816fd318371e954dc5df9
pedro <pedropkmmt@gmail.com>
2023-11-17 19:00:44 -0800

    fixes

--- main.h
+++ main.h
@@ -1,12 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+
+
+/**
+ * struct format - converter for printf
+ * @ph: type char pointer of the specifier
+ * @function: function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *ph;
+	int (*function)();
+} convert;
+int _putchar(char c);
 int _printf(const char *format, ...);
-void print_buffer(char buffer[], int *buff_ind);
-int print_char(va_list types, char buffer[],int flags, int width, int precision, int size)
-int print_percent(va_list types, char buffer[],int flags, int width, int precision, int size)
-
-#endif
+int print_37(void);
+int print_c(va_list val);
+int print_s(va_list val);
